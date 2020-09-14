53189db5f1bdd9ed80eca787585e0df52e25b970
Vladyslav Dukhin <vladyslav.dukhin@gmail.com>
2020-09-15 00:56:42 +0300

    chore: add prettier configuration file

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: "consistent",
+  jsxSingleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "avoid",
+  endOfLine: "lf",
+  embeddedLanguageFormatting: "off",
+};
